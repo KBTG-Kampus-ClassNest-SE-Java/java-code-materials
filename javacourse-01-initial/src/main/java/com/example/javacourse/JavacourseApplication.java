860e3f83a3b555e10ac890937eebf308c94bd104
Chin.K <chinwat@odds.team>
2023-10-07 21:23:19 +0700

    init-01

---
+++ javacourse-01-initial/src/main/java/com/example/javacourse/JavacourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.javacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavacourseApplication.class, args);
+	}
+
+}
