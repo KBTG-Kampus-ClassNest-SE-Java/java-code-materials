46e6aa302b8ab88f02535832d72efaa9567e18a0
Chin.K <chinwat@odds.team>
2023-10-28 10:52:23 +0700

    init-sec-1

---
+++ java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/SecuritycourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitycourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritycourseApplication.class, args);
+	}
+
+}
---
+++ java-code-security/securitycourse-01-solved/src/test/java/com/example/securitycourse/SecuritycourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securitycourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritycourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
