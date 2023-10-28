cef2bdfde60d19eb73cd4af76b2d09d6b50e22d9
Chin.K <chinwat@odds.team>
2023-10-28 11:26:30 +0700

    02-create-own-filter-chain

---
+++ java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/SpringSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.securitycourse;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+@EnableWebSecurity
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
