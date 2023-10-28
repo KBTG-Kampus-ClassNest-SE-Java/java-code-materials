28f00c925df3381d018d6f42bac807163fd3fcfa
Chin.K <chinwat@odds.team>
2023-10-28 11:55:59 +0700

    04-api-key

---
+++ java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/ApiKeyAuthFilter.java
@@ -0,0 +1,32 @@
+package com.example.securitycourse;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
+    private final String HEADER_NAME = "X-API-KEY";
+    private final String API_KEY = "your-api-key"; // This should be stored securely and not hardcoded
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String apiKey = request.getHeader(HEADER_NAME);
+        if (StringUtils.hasLength(apiKey) && API_KEY.equals(apiKey)) {
+            // The token is valid, so we set the authentication in the security context
+            SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(apiKey, null, Collections.emptyList()));
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/SpringSecurityConfig.java
+++ java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/SpringSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -17,9 +18,10 @@ public class SpringSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
-        http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
-        return http.build();
+        return http
+                .authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .addFilterBefore(new ApiKeyAuthFilter(), BasicAuthenticationFilter.class)
+                .httpBasic(withDefaults())
+                .build();
     }
 }
