3026fb715d58e62adb1fb6ed49c5a06625addcff
Chin.K <chinwat@odds.team>
2023-10-28 22:52:22 +0700

    06-use-own-user-detail

--- java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/controller/MemberController.java
+++ java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.example.securitycourse.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,12 @@ public class MemberController {
 
     @GetMapping("")
     public String getResource() {
-        return "Member Resource";
+        return "Get Member Resource";
+    }
+
+    @PutMapping("")
+    public String updateResource() {
+        return "Update Member Resource";
     }
 
 }
--- java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/security/SpringSecurityConfig.java
+++ java-code-security/securitycourse-01-solved/src/main/java/com/example/securitycourse/security/SpringSecurityConfig.java
@@ -3,8 +3,17 @@ package com.example.securitycourse.security;
 import com.example.securitycourse.security.ApiKeyAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -18,13 +27,28 @@ public class SpringSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
+//                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) ->
                         requests
+                                .requestMatchers(HttpMethod.GET,"/member").hasAuthority("MEMBER_READ")
+                                .requestMatchers(HttpMethod.PUT,"/member").hasAuthority("MEMBER_UPDATE")
                                 .requestMatchers("/public/**").permitAll()
                                 .anyRequest()
                                 .authenticated())
                 .addFilterBefore(new ApiKeyAuthFilter(), BasicAuthenticationFilter.class)
                 .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetail() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        UserDetails user = User.withUsername("member1")
+                .password(encoder.encode("password"))
+                .authorities("MEMBER_READ", "MEMBER_UPDATE")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
