ebc0fd4dc0f94d26186f57668ac62a9d99938c44
WeRockStar <kotchaphan.m@gmail.com>
2023-11-04 09:37:05 +0700

    Remove unused code

--- api-design-database/01-api-desgin/ch-05-javacourse-05-service-impl-02-mail-tigh-coupling/src/main/java/com/example/javacourse/JavacourseApplication.java
+++ api-design-database/01-api-desgin/ch-05-javacourse-05-service-impl-02-mail-tigh-coupling/src/main/java/com/example/javacourse/JavacourseApplication.java
@@ -3,8 +3,6 @@ package com.example.javacourse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SpringBootApplication
 public class JavacourseApplication {
