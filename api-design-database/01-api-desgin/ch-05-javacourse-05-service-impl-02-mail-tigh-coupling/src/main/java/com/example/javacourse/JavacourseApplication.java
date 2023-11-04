24bd8fde8dd00b963d8df4b3bebc08a2348268f4
Chin.K <chinwat@odds.team>
2023-11-04 09:44:25 +0700

    remove comment

--- api-design-database/01-api-desgin/ch-05-javacourse-05-service-impl-02-mail-tigh-coupling/src/main/java/com/example/javacourse/JavacourseApplication.java
+++ api-design-database/01-api-desgin/ch-05-javacourse-05-service-impl-02-mail-tigh-coupling/src/main/java/com/example/javacourse/JavacourseApplication.java
@@ -8,18 +8,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class JavacourseApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(JavacourseApplication.class, args);
+		SpringApplication.run(JavacourseApplication.class, args);
 	}
-
-//	mappingJackson2HttpMessageConverter
-
-
-//	change in postman raw
-//	@Bean
-//	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-//		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-//		converter.setPrettyPrint(true);
-//		return converter;
-//	}
-
 }
